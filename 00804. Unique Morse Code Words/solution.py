df9598e4866b39da4b02b4a068a227721cb3a7b9
katsumeshi <katsumeshi@gmail.com>
2020-03-10 09:02:51 -0700

    Create solution.py

---
+++ 00804. Unique Morse Code Words/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        for i in range(len(words)):
+            m = ""
+            for c in words[i]:
+                m += morse[ord(c)-97]
+            words[i] = m
+        return len(set(words)) 
