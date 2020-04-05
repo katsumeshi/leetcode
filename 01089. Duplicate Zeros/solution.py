bae4b8588b90b284fc2eee62247bf66665099555
katsumeshi <katsumeshi@gmail.com>
2020-04-04 17:08:29 -0700

    Create solution.py

---
+++ 01089. Duplicate Zeros/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: None Do not return anything, modify arr in-place instead.
+        """
+        deque(arr)
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i, 0)
+                arr.pop()
+                i += 1
+            i += 1
